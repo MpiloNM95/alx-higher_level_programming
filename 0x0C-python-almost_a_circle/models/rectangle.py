f00beb910a0944e7c3275ca4a78a6f21211afedc
MpiloNM95 <mpilonmafu95@gmail.com>
2022-10-30 03:02:43 -0700

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,3 +26,8 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """Set/get the width of the Rectangle."""
+        return self.__width
