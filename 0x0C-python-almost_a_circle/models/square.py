4caaac8ea1d3fddc78db9db0a45b963a9b80777c
MpiloNM95 <mpilonmafu95@gmail.com>
2022-10-30 05:32:28 -0700

    done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,3 +16,8 @@ class Square(Rectangle):
             id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
