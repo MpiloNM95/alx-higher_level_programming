1d9635e377b8cf557964ec2fe8c48bb82b30a776
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-30 05:15:49 -0800

    task 0

--- 0x0E-SQL_more_queries/0-priviledges.sql
+++ 0x0E-SQL_more_queries/0-priviledges.sql
@@ -1,4 +1,4 @@
--- lists all priviledges of users
--- on MySQl server
-SHOW GRANTS FOR user_0d_1@locahost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- Lists all privileges of the MySQL users 
+-- user_0d_1 and user_0d_2 on your server (in localhost).
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
