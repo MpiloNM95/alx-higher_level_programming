bfdb633aee4eacaf10b4a910d36a93d40fd303d7
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-30 05:01:16 -0800

    task 0

--- 0x0E-SQL_more_queries/0-priviledges.sql
+++ 0x0E-SQL_more_queries/0-priviledges.sql
@@ -1,4 +1,4 @@
 -- lists all priviledges of users
 -- on MySQl server
-SHOW GRANTS FOR 'user_0d_1'@'locahost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANT FOR 'user_0d_1'@'locahost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
