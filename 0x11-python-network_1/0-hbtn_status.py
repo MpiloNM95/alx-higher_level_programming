75ecffad3d0b2a796a2b207777838f221219b4c5
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-17 03:14:02 -0800

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,9 @@
 """Python code that check intranet status"""
 
 
-import urllib.request
-
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(type(content), content, content.decode('utf-8')))
