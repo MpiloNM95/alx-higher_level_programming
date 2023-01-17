6b3c189674ebcad1c9b6f9c3d5bca769e9be9e46
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-17 03:22:59 -0800

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,9 @@
 
 
 import requests
+
 if __name__ == "__main__":
-    request = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-            .format(type(request.text), request.text))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
