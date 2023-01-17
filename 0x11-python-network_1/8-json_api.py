8ee1be9463ac6b37a9d78fe30abe7554c7dfad8c
MpiloNM95 <mpilonmafu95@gmail.com>
2023-01-17 03:34:56 -0800

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """ Script that takes in a letter and send it """
-
-
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
-    else:
-        q = ""
 
-    values = {'q': q}
-    request = requests.post('http://0.0.0.0:5000/search_user', data=values)
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = request.json()
-        if response:
-            print("[{}] {}".format(response['id'], response['name']))
-        else:
+        response = r.json()
+        if response == {}:
             print("No result")
-    except:
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
         print("Not a valid JSON")
