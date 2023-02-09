a0d894d42f4f24361e2aa91e5da7f3061c9610d5
MpiloNM95 <mpilonmafu95@gmail.com>
2023-02-09 04:49:37 -0800

    fixed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,16 @@
 #!/usr/bin/node
-const argv = process.argv;
-const url = argv[2];
 const request = require('request');
-const dict = {};
-
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const results = JSON.parse(body);
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].completed === true) {
-	if (results[i].userId in dict) {
-	} else {
-	  dict[results[i].userId] = 1;
-	}
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+	completed[todo.userId] = 1;
+      } else if (todo.completed) {
+	completed[todo.userId] += 1;
       }
-    }
-    console.log(dict);
+    });
+    console.log(completed);
   }
 });
