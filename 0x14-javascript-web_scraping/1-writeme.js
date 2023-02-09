f6c08fe3c87f14c298353e2957bcca9746ff19a6
MpiloNM95 <mpilonmafu95@gmail.com>
2023-02-09 04:05:35 -0800

    hot fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-const argv = process.argv;
 const fs = require('fs');
-fs.writeFile(argv[2], argv[3], function (err) {
-	if (err) {
-		return console.log(err);
-	}
-});
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+}):
